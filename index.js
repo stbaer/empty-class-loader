89c907a3927259f63c68be7846e48b2c006aecb0
Steffen Baer <stbaer.mail@gmail.com>
2017-08-19 11:37:56 +0800

    add destroy, fix getClassName

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var returnVal = function(className) {
-  "exports.__esModule = true;" +
-  "var " + className + " = function () {function " + className + "() {} };" +
-    "exports[" + className + "] = " + className + ";"
-  "exports.default = " + className + ";"
+  return "exports.__esModule = true;" +
+    "var " + className + " = function () {function " + className + "() {} };" +
+		className + ".prototype.destroy = function () {};" +
+    "exports." + className + " = " + className + ";" +
+    "exports.default = " + className + ";"
 }
 
 var getClassName = function(filePath) {
   var splitPath = filePath.split('/');
-  var className = splitPath[splitPath.length - 1].split('.')[0];
+  return splitPath[splitPath.length - 1].split('.')[0];
 }
 
 module.exports.pitch = function(filePath) {
